46fc9052a1a08eb5ca132b0245b5271ce191c2a3
Kyrylo Tykhonov <tikhonov_kirill@outlook.com>
2023-02-19 13:59:22 +0200

    new commit

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 const formElement = document.querySelector('form')
+const listBlock = document.getElementById('list-block')
+var enteredMassage
 
 
 function saveNote(event) {
     event.preventDefault()
 
     const formData = new FormData(event.target)
-    const userTextInput = formData.get('user-input').trim()
+    const userTextInput = formData.get('userinput').trim()
+    enteredMassage = userTextInput
+    console.log(enteredMassage)
 
     if (!userTextInput) {
         alert('You didn\'t write anything!')
         return
     }
 
-    const listElements = document.getElementById('list-block')
-    const newListElement = listElements.appendChild()
+    createNewListItem()
+}
 
+function createNewListItem(event) {
+    const newListItem = document.createElement('li')
+    const newParagraph = document.createElement('p')
+
+    listBlock.appendChild(newListItem)
+    newListItem.appendChild(newParagraph)
+    newParagraph.textContent = enteredMassage
+
+    /* const textArea = document.getElementById('text-area')
+    textArea.value = '' */
 }
 
 formElement.addEventListener('submit', saveNote)
