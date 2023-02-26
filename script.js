ff8b5fe07c4e142b0cddc2ef9d995a02b0745296
Kyrylo Tykhonov <tikhonov_kirill@outlook.com>
2023-02-26 12:36:52 +0200

    new commit

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
-const formElement = document.querySelector('form')
-const listBlock = document.getElementById('list-block')
-const deleteBtn = document.getElementById('123')
+const formElement = document.querySelector('form');
+const listBlock = document.getElementById('list-block');
+const deleteBtn = document.getElementById('123');
 
-var enteredMassage
+var enteredMassage;
 
 
 function saveNote(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const formData = new FormData(event.target)
-    const userTextInput = formData.get('userinput').trim()
-    enteredMassage = userTextInput
+    const formData = new FormData(event.target);
+    const userTextInput = formData.get('userinput').trim();
+    enteredMassage = userTextInput;
 
     if (!userTextInput) {
-        alert('You didn\'t write anything!')
-        return
-    }
+        alert('You didn\'t write anything!');
+        return;
+    };
 
-    createNewListItem()
-}
+    createNewListItem();
+};
 
 function createNewListItem(event) {
-    const newListItem = document.createElement('li')
-    const newDivElement = document.createElement('div')
-    const newParagraph = document.createElement('p')
-    const newBtnDeleteElement = document.createElement('button')
-    /* const newBtnEditNote = document.createElement('button') */
+    const newListItem = document.createElement('li');
+    const newDivElement = document.createElement('div');
+    const newParagraph = document.createElement('p');
+    const newBtnDeleteElement = document.createElement('button');
+    /* const newBtnEditNote = document.createElement('button'); */
 
-    listBlock.append(newListItem)
-    newListItem.append(newDivElement)
-    newDivElement.appendChild(newParagraph)
-    /* newDivElement.appendChild(newBtnEditNote) */
-    newDivElement.appendChild(newBtnDeleteElement)
-    newParagraph.textContent = enteredMassage
+    listBlock.append(newListItem);
+    newListItem.append(newDivElement);
+    newDivElement.appendChild(newParagraph);
+    /* newDivElement.appendChild(newBtnEditNote); */
+    newDivElement.appendChild(newBtnDeleteElement);
+    newParagraph.textContent = enteredMassage;
 
-    newListItem.classList.add('listElement')
+    newListItem.classList.add('listElement');
 
-    newBtnDeleteElement.textContent = 'X'
+    newBtnDeleteElement.textContent = 'X';
     newBtnDeleteElement.addEventListener('click', () => {
-        newListItem.remove()
-    })
+        newListItem.remove();
+    });
 
-    /* newBtnEditNote.textContent = 'Edit'
-    newBtnEditNote.onclick = function () { editNote() } */
+    /* newBtnEditNote.textContent = 'Edit';
+    newBtnEditNote.onclick = function () { editNote() }; */
 
-    const textArea = document.getElementById('text-area')
-    textArea.value = ''
-    enteredMassage.value = ''
+    const textArea = document.getElementById('text-area');
+    textArea.value = '';
+    enteredMassage.value = '';
 }
 
-formElement.addEventListener('submit', saveNote)
+formElement.addEventListener('submit', saveNote);
